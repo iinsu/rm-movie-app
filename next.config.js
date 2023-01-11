74dd6c0150d6bab6e89924d384bd4ee72987046a
iinsue <hinsu@rmsoft.kr>
2023-01-11 19:33:35 +0900

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   compiler: {
     styledComponents: true,
+  },
+  images: {
+    domains: ["image.tmdb.org"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/:id",
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
+      },
+    ];
   },
 };
 
