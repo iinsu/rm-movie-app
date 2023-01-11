2aff57f83067acdadaa4c0944b98fd0f0b15591b
iinsue <hinsu@rmsoft.kr>
2023-01-11 17:58:25 +0900

    set recoil & react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,23 @@
+import {
+  QueryClientProvider,
+  QueryClient,
+  Hydrate,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Hydrate state={pageProps.results}>
+          <ReactQueryDevtools initialIsOpen={true} />
+          <Component {...pageProps} />
+        </Hydrate>
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
 }
